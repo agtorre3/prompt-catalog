9dec082885bbaf0fa8730e26b2a92b7739e0932c
Alejandra Torres <agtorre3@yahoo.com>
2025-09-08 23:50:15 -0700

    Add Eb Garamond font, add to font demo, add font demo page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::Base
+  # Font demo action (remove after testing)
+  def font_demo
+    render 'layouts/font_demo'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   resources :relationships, only: [:create]
   resources :traits, only: [:index]
   resources :character_traits, only: [:create]
+  
+  # Font demo route (remove after testing)
+  get "font-demo" => "application#font_demo", as: :font_demo
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
