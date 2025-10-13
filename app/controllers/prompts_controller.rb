101e96f44ef7ec0cdccdc62bd81bd7ff7cc52b4e
Alejandra Torres <agtorre3@yahoo.com>
2025-10-12 21:26:52 -0700

    Dark theme cards

--- app/controllers/prompts_controller.rb
+++ app/controllers/prompts_controller.rb
@@ -4,7 +4,7 @@ class PromptsController < ApplicationController
 
   # GET /prompts or /prompts.json
   def index
-    @prompts = Prompt.all
+    @prompts = Prompt.includes(:tags, :characters, :relationships, :character_traits, :user).all
   end
 
   # GET /prompts/1 or /prompts/1.json
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -5,4 +5,8 @@ class Tag < ApplicationRecord
   has_many :prompts, through: :prompt_tags
 
   validates :name, presence: true, uniqueness: { scope: :tag_category_id }
+
+  scope :visible, -> { where.not(tag_category_id: TagCategory.find_by(name: "NSFW").id)
+                      .where.not(tag_category_id: TagCategory.find_by(name: "Warnings").id)
+                      .where.not(tag_category_id: TagCategory.find_by(name: "Character").id) }
 end
